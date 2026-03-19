d26fa41a3845adfccedd9d148ab07aa7a59c13e4
Artem Shimko <a.shimko@yadro.com>
2026-03-19 14:59:36 +0300

    add workspace notes for yadro

--- install.sh
+++ install.sh
@@ -9,4 +9,8 @@ chmod 700 ~/.ssh
 ln -sf ~/dotfiles/.ssh/config ~/.ssh/config
 chmod 600 ~/.ssh/config
 
+#workspace
+mkdir -P ~/workspace
+ln -sf ~/dotfiles/workspace_yadro.txt ~/workspace/workspace_yadro.txt
+
 echo "Dotefiles installed"
