53f11c8f8359c0da83b94a9b77d1b0b9775403de
Artem Shimko <a.shimko@yadro.com>
2026-03-27 11:50:24 +0000

    fix errors, tested on raspberry

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -e
 
+INSTALL_SSH=true
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+	--help)
+		echo "--ssh_install_ex to exclude ssh setup"
+		exit 1
+		;;
+        --ssh_install_ex)
+		INSTALL_SSH=false
+		shift
+		;;
+	*)
+		echo "Unknown option: $1"
+		exit 1
+		;;
+	esac
+done
+
 read -p "Go (y/n) " -n 1 -r
 echo
 
@@ -9,28 +28,27 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
 
+BACKUP_DATE=$(date +"%d%m%Y_%H%M%S")
+echo ${BACKUP_DATE}
+
 sudo apt update
-
-echo "---------- git ----------"
-git --version
-sudo apt install -y git-email
-if [ -f ~/.gitconfig]; then
-	mv ~/.gitconfig ~/.gitconfig.dotfilebak
-fi
-
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-echo "git config 🧙"
-echo "----------     ----------"
+sudo apt upgrade -y
 
 echo "---------- vim ----------"
 if ! command -v vim &> /dev/null; then
 	sudo apt install -y vim
 fi
 vim --version | head -n 1
+
+if ! command -v ctag &> /dev/null; then
+	sudo apt install -y universal-ctags
+fi
+ctags --version
+
 echo "vim installing 🧙"
 
-if [ ! -f ~/.vimrc ]; then
-	mv ~/.vimrc ~/.vimrc.dotfilebak
+if [ -f ~/.vimrc ]; then
+	mv ~/.vimrc ~/.vimrc.${BACKUP_DATE}.dotfilebak
 fi
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 echo "vim config 🧙"
@@ -42,8 +60,8 @@ if ! command -v tmux &> /dev/null; then
 fi
 echo "tmux installing 🧙"
 
-if [ ! -f ~/.tmux.conf ]; then
-        mv ~/.tmux.conf ~/.tmux.conf.dotfilebak
+if [ -f ~/.tmux.conf ]; then
+        mv ~/.tmux.conf ~/.tmux.conf.${BACKUP_DATE}.dotfilebak
 fi
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 echo "tmux config 🧙"
@@ -51,7 +69,7 @@ echo "----------    ----------"
 
 echo "---------- workspace ----------"
 if [ -f ~/workspace/workspace_yadro.txt ]; then
-	mv ~/workspace/workspace_yadro.txt ~/workspace/workspace_yadro.txt.dotfilebak
+	mv ~/workspace/workspace_yadro.txt ~/workspace/workspace_yadro.txt.${BACKUP_DATE}.dotfilebak
 else
 	mkdir -p ~/workspace
 fi
@@ -59,36 +77,49 @@ fi
 ln -sf ~/dotfiles/workspace_yadro.txt ~/workspace/workspace_yadro.txt
 echo "----------           ----------"
 
-echo "---------- ssh ----------"
-if ! command -v ssh &> /dev/null; then
-        sudo apt install -y openssh-client
+if [[ ${INSTALL_SSH} == true ]]; then 
+	echo "---------- ssh ----------"
+	if ! command -v ssh &> /dev/null; then
+		sudo apt install -y openssh-client
+	fi
+	echo "ssh installing 🧙"
+
+	mkdir -p ~/.ssh
+	chmod 700 ~/.ssh
+
+	if [ -f ~/.ssh/id_rsa ]; then
+		mv ~/.ssh/id_rsa ~/.ssh/id_rsa.${BACKUP_DATE}.dotfilebak
+		mv ~/.ssh/id_rsa.pub ~/.ssh/id_rsa.pub.${BACKUP_DATE}.dotfilebak
+	fi
+
+	ssh-keygen -t rsa
+	echo "ssh id_rsa 🧙"
+
+	if [ -f ~/.ssh/config ]; then
+		mv ~/.ssh/config ~/.ssh/config.${BACKUP_DATE}.dotfilebak
+	fi
+
+	ln -sf ~/dotfiles/.ssh/config ~/.ssh/config
+	chmod 600 ~/.ssh/config
+	echo "ssh config 🧙"
+	echo "----------     ----------"
 fi
-echo "ssh installing 🧙"
 
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-
-if [ -f ~/.ssh/id_rsa ]; then
-        mv ~/.ssh/id_rsa ~/.ssh/id_rsa.dotfilebak
-        mv ~/.ssh/id_rsa.pub ~/.ssh/id_rsa.pub.dotfilebak
-        ssh-keygen -t rsa
+echo "---------- git ----------"
+git --version
+sudo apt install -y git-email
+if [ -f ~/.gitconfig]; then
+	mv ~/.gitconfig ~/.gitconfig.${BACKUP_DATE}.dotfilebak
 fi
 
-echo " id_rsa.pub for GitHub/GitLab/??"
-echo "VVvvvvVV"
-cat ~/.ssh/id_rsa.pub
-echo "^^^^^^^^"
-echo "ssh id_rsa 🧙"
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
-if [ -f ~/.ssh/config]; then
-        mv ~/.ssh/config ~/.ssh/config.dotfilebak
-fi
+git config --global url."git@github.com:".insteadOf "https://github.com/"
 
-ln -sf ~/dotfiles/.ssh/config ~/.ssh/config
-chmod 600 ~/.ssh/config
-echo "ssh config 🧙"
-echo "----------     ----------
+echo "git config 🧙"
+echo "----------     ----------"
 
-
-find ~ -name "*.dotfilebak*" -type f 
+find ~/ -name "*.dotfilebak*" -type f
 echo "Dotefiles installed"
+
+cat ~/.ssh/id_rsa.pub
